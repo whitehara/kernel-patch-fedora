ff2d88ce245599a68c233f913327ae8fe55a1f36
whitehara <white@vx-xv.com>
2025-05-10 08:44:31 +0900

    Add the script for getting a new cachyos patch

---
+++ build-script/get-cachos-patch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+VER=6.14
+cd ../$VER
+wget https://raw.githubusercontent.com/CachyOS/kernel-patches/refs/heads/master/$VER/all/0001-cachyos-base-all.patch
+sed -i -e "s/BUFSIZE 256/BUFSIZE 4096/g" 0001-cachyos-base-all.patch.1
+diff -u --color 0001-cachyos-base-all.patch 0001-cachyos-base-all.patch.1
