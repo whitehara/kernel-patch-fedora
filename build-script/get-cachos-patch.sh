c593ff0b73d8201c713a9471c5f7bcadcee19cce
whitehara <white@vx-xv.com>
2025-10-30 10:07:08 +0900

    Get version info from the command line option

--- build-script/get-cachos-patch.sh
+++ build-script/get-cachos-patch.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-VER=6.17
+if [ ! -d ../$1 ]; then
+	echo "No such directory: ../$1"
+	exit 1
+else
+	VER=$1
+fi
+
 cd ../$VER
-wget https://raw.githubusercontent.com/CachyOS/kernel-patches/refs/heads/master/$VER/all/0001-cachyos-base-all.patch
-sed -i -e "s/BUFSIZE 256/BUFSIZE 4096/g" 0001-cachyos-base-all.patch.1
+wget https://raw.githubusercontent.com/CachyOS/kernel-patches/refs/heads/master/$VER/all/0001-cachyos-base-all.patch && \
+sed -i -e "s/BUFSIZE 256/BUFSIZE 4096/g" 0001-cachyos-base-all.patch.1 && \
 diff -u --color 0001-cachyos-base-all.patch 0001-cachyos-base-all.patch.1
